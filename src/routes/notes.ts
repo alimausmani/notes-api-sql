0d70753a49ae8db53721d4211437b8b177394083
Alima Usmani <alimausmani21@navgurukul.org>
2026-04-22 23:49:26 +0530

    update notes routes to include tags in request body and database insertion and created get notes api

--- src/routes/notes.ts
+++ src/routes/notes.ts
@@ -4,27 +4,45 @@ import { pool } from "../db/db";
 
 export async function notesRoutes(app: FastifyInstance) {
 
+  app.get('/notes', {
+    schema: {
+      tags: ['Notes']
+    }
+  }, async () => {
+    const result = await pool.query(
+      `SELECT id, user_id, title, content, tags__
+       FROM notes
+       ORDER BY id DESC`
+    );
+
+    return result.rows;
+  });
+
   app.post('/notes', {
     schema: {
       tags: ['Notes'],
       body: {
         type: 'object',
-        required: ['title', 'content', 'userId'],
+        required: ['title', 'content', 'tags', 'userId'],
         properties: {
           title: { type: 'string' },
           content: { type: 'string' },
+          tags: {
+            type: 'array',
+            items: { type: 'string' }
+          },
           userId: { type: 'number' }
         }
       }
     }
   }, async (req: any) => {
 
-    const { title, content, userId } = req.body;
+    const { title, content, tags, userId } = req.body;
 
     const result = await pool.query(
-      `INSERT INTO notes (user_id,title,content)
-       VALUES ($1,$2,$3) RETURNING id`,
-      [userId, title, content]
+      `INSERT INTO notes (user_id,title,content,tags)
+       VALUES ($1,$2,$3,$4) RETURNING id`,
+      [userId, title, content, tags]
     );
 
     return result.rows[0];
